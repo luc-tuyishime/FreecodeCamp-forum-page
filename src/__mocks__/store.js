ee1d70d2c6882055fa49a2cc70e0f218a6436b68
@luc-tuyishimee <luctunechi45@gmail.com>
2020-10-28 22:52:06 +0200

    add unit test for Post component

---
+++ src/__mocks__/store.js
@@ -0,0 +1,21 @@
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
+import initialState from "../redux/store/initialState";
+import apiMiddleware from "../middleware/apiMiddleware";
+
+export { initialState };
+export const middlewares = [thunk, apiMiddleware];
+export const mockStore = configureMockStore(middlewares);
+export const adminStore = mockStore({
+    ...initialState,
+    user: {
+        ...initialState.user,
+        isAuth: true,
+        profile: { ...initialState.user.profile, role: "admin" }
+    }
+});
+
+export default mockStore({
+    ...initialState,
+    user: { ...initialState.user, isAuth: true }
+});
